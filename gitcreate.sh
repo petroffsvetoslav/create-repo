f8dcd228a300548fdac19dcfcfb8fd27d3fb9f0c
Svetoslav Petrov <petroffsvetoslav@gmail.com>
2021-02-23 21:35:14 +0200

    implemeting encrypted file

--- gitcreate.sh
+++ gitcreate.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 
-#VARIABLES
-
-#Configure your API tocken here
-echo -n "Paste API tocken here: "
-read -r GH_API_TOKEN
-
-#Set name for the new repository
-NEW_REPO_NAME="zazz"
-
-#Configure your Github username and email address
+#Variables
+NEW_REPO_NAME="zzz"
 GIT_USER="Svetoslav Petrov"
 GIT_EMAIL="petroffsvetoslav@gmail.com"
 
-#Create git project dir
-mkdir -p ~/Desktop/"$NEW_REPO_NAME" && cd ~/Desktop/"$NEW_REPO_NAME"
+#File name that will contain sensitive data
+FILENAME=token.conf
 
-#Create the repo by making an API call
-curl -H "Authorization: token $GH_API_TOKEN" https://api.github.com/user/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}'
+#Create local project directory
+mkdir -p ~/Desktop/"$NEW_REPO_NAME"
 
-#Config git repository
+#Configure your API tocken on this step
+echo -n "Paste API tocken here: "
+read -r GH_API_TOKEN
+echo "$GH_API_TOKEN" > "$FILENAME"
+
+#Encryptin the sensitive data file
+echo "Configure password for the sensitive data file"
+openssl enc -aes-256-cbc -pbkdf2 -iter 20000 -in "$FILENAME" -out "$FILENAME".enc
+
+#Decrypt the sensitive data file
+echo "Decrypt the sensitive data file"
+xargs -a "$FILENAME" openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in "$FILENAME".enc -out "$FILENAME" > /dev/null 2>&1
+
+#Make API call
+LINES=$(cat $FILENAME)
+
+for LINE in $LINES
+do
+    curl -H "Authorization: token $LINE" https://api.github.com/user/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}'
+done
+
+#Remove the sensitive data file after call
+rm "$FILENAME"
+
+#Change to new project directory
+cd ~/Desktop/"$NEW_REPO_NAME"
+
+#Config remote git repository
 echo "$NEW_REPO_NAME" >> README.md
 git init
 git config user.name "$GIT_USER"
 git config user.email "$GIT_EMAIL"
 git add README.md
-git commit -m "adding README file"
+git commit -m "adding README file and .gitignore"
 git branch -M master
 git remote add origin git@github.com:petroffsvetoslav/"$NEW_REPO_NAME".git
 git push -u origin master
